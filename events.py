03cba9cc1aa6e1b1badc2f5daf0022e6c7d7c868
arunpatyal <Sumologicarun1983>
2018-11-13 13:06:35 +0530

    events scirpt

--- events.py
+++ events.py
@@ -8,36 +8,29 @@ import datetime
 import socket
 import argparse
 import atexit
-from pyVmomi import vim, vmodl
-from pyVim.connect import SmartConnectNoSSL,Disconnect
-import json
-import simplejson
+import ssl
+from pyVmomi import vim
+from pyVim.connect import SmartConnect, Disconnect
+from cryptography.fernet import Fernet
 
 
-def prepareQueryTimeRange(tsFileName,optBeginTime,optEndTime):
+def prepareQueryTimeRange(tsFileName, optBeginTime, optEndTime, logfile, logPrefix):
 
     endTime = datetime.datetime.now()
     beginTime = endTime - datetime.timedelta(days=1)
     if optEndTime is not None:
-        endTime =  datetime.datetime.strptime(optEndTime, '%Y-%m-%d %H:%M:%S.%f%z')
+        endTime = datetime.datetime.strptime(optEndTime, '%Y-%m-%d %H:%M:%S.%f%z')
 
     if optBeginTime is not None:
         beginTime = datetime.datetime.strptime(optBeginTime, '%Y-%m-%d %H:%M:%S.%f%z')
-        # beginTime = int(one_day_ago.timestamp() * 1000)
     else:
         try:
             with open(tsFileName, 'r') as timestampFile:
                 for line in timestampFile:
-                    print("aaa %s"%line)
                     beginTime = datetime.datetime.strptime(line, '%Y-%m-%d %H:%M:%S.%f%z')
-                    print("bbbb %s"%beginTime)
-                    #if re.match(r'^#', line) is not None:
-                    #    beginTime = re.sub(r'[\n]+', r'', line)
-                    #    print("Inside if %s" %begintime)
                     break
-            # timestampFile.close()
-        except:
-            print('Time log not found, will get events 1 day back.')
+        except Exception:
+            logfile.write(logPrefix + 'Time log not found, will get events 1 day back.\n')
 
     return beginTime, endTime
 
@@ -54,12 +47,12 @@ def setup_args():
     parser.add_argument('-s', '--server',
                         required=True,
                         action='store',
-                        help='Remote ESXi|vCenter Server to connect to')
+                        help='Remote vCenter Server to connect to')
 
     parser.add_argument('-o', '--port',
                         required=False,
                         action='store',
-                        help="Remote ESXi|vCenter Server port to use, default 443", default=443)
+                        help="Remote vCenter Server port to use, default 443", default=443)
 
     parser.add_argument('-u', '--user',
                         required=True,
@@ -74,12 +67,12 @@ def setup_args():
     parser.add_argument('-f', '--file',
                         required=False,
                         action='store',
-                        help='Output Log File.')
+                        help='Output File Prefix.')
 
     parser.add_argument('-ts', '--timestampFile',
                         required=False,
                         action='store',
-                        help='Timestamp File.')
+                        help='Timestamp File.', default='timelog_events')
 
     parser.add_argument('-t', '--target',
                         required=False,
@@ -101,195 +94,176 @@ def setup_args():
                         action='store',
                         help='End Time to query for the events.')
 
+    parser.add_argument('-sV', '--ssl_verify',
+                        required=False,
+                        action='store',
+                        help='Use SSL for connection.', default=False)
+
+    parser.add_argument('-sC', '--ssl_capath',
+                        required=False,
+                        action='store',
+                        help='SSL cert for connection.', default=None)
+
+    parser.add_argument('-l', '--log_file_prefix',
+                        required=False,
+                        action='store',
+                        help='Log File.', default='vsphere_events_')
+
+    parser.add_argument('-pE', '--pass_encrypted',
+                        required=False,
+                        action='store',
+                        help='Is the password encrypted?', default=False)
+
+    parser.add_argument('-pK', '--key',
+                        required=False,
+                        action='store',
+                        help='Encryption Key', default=False)
+
     args = parser.parse_args()
 
     if args.target is None and args.file is None:
         sys.exit("Target syslog server or file is required.")
+
+    if args.pass_encrypted and args.key is None:
+        sys.exit("Key is required if the password is encrypted.")
 
     args = parser.parse_args()
 
     return args
 
 
-def convertToDict(obj, oF):
-
-    data = {}
-    if not obj:
-        return ""
-    if type(obj) in (str, int, float, datetime.datetime, bool):
-        return str(obj)
-    if obj.__class__.mro()[1].__name__ == "Enum":
-        return str(obj)
-    for k in dir(obj):
-
-        if k.startswith(("__", "_")):
-            continue
-        try:
-            v = getattr(obj, k)
-        except vmodl.fault.ManagedObjectNotFound:
-            continue
-        #if hasattr(obj, "_moId") and hasattr(v, "_moId") and obj == v:
-        #    data[k] = str(v)
-        # if str(obj) == str(k) == str(v): fails in green and gray
-
-        if type(v) is type or callable(v):
-            continue
-        oF.write("Keys is::" + str(k) + "::Values is::" + str(v))
-        if type(v) is list:
-            all_records = []
-            for record in v:
-                all_records.append(convertToDict(record,oF))
-            data[k] = all_records
-        elif type(v) is dict:
-            all_records_d = []
-            for key, record in v.items():
-                # print(convertToDict(record))
-                all_records_d.append(convertToDict(record,oF))
-            data[k] = all_records_d
-        elif type(v) in (str, int, float, datetime.datetime, bool):
-            data[k] = str(v)
-        else:
-            data[str(k)] = convertToDict(v,oF)
-    return data
-
-
-
 def main():
     args = setup_args()
+    logPrefix = "server: " + args.server + " >>> "
 
-    if args.timestampFile is not None:
-        tsFileName = args.timestampFile
-    else:
-        tsFileName = "timelog"
-
-    if args.server is not None:
-        logPrefix = "server: " + args.server + " >>> "
+    try:
+        logfilename = args.log_file_prefix + "_" + str(datetime.datetime.now().timestamp()) + ".log"
+        logfile = open(logfilename, 'w+')
+    except Exception as e:
+        sys.exit(logPrefix + "Unable to open logfile %s:: Error:: %s" % (logfilename, e))
 
     if args.file == "-":
         outputFile = sys.stdout
     else:
         try:
-            outputFile = open(args.file, 'w+')
-        except:
-            # print("Unable to open %s" % args.file)
-            sys.exit(logPrefix + "Unable to open %s" % (args.file))
+            outfilename = args.file + "_" + str(datetime.datetime.now().timestamp()) + ".evt.out"
+            outputFile = open(outfilename, 'w+')
+        except Exception as e:
+            logfile.write(logPrefix + "Unable to open %s\n" % args.file)
+            sys.exit(logPrefix + "Unable to open output file %s:: Error:: %s" % (args.file, e))
 
     # Prepare socket to send
     targetSocket = None
     if args.target is not None:
         try:
-            print(logPrefix + "Attempting to connect to TCP log server")
             targetSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            print(logPrefix + "Socket successfully created")
         except socket.error as err:
+            logfile.write(logPrefix + " Socket creation failed with error %s\n" % (err))
             sys.exit(logPrefix + " Socket creation failed with error %s" % (err))
 
         try:
             # connecting to the server
             targetSocket.connect((args.target, args.targetPort))
-            print(logPrefix + "The socket has successfully connected to server %s " % (args.target))
         except socket.error as err:
+            logfile.write(logPrefix + "Target server connection failed with error %s\n" % (err))
             sys.exit(logPrefix + "Target server connection failed with error %s" % (err))
 
-   # Prepare time range for query
+    #  Prepare time range for query
 
-    beginTime , endTime = prepareQueryTimeRange(tsFileName,args.optBeginTime,args.optEndTime)
+    beginTime, endTime = prepareQueryTimeRange(args.timestampFile, args.optBeginTime, args.optEndTime, logfile, logPrefix)
 
-    print("1111%s" %beginTime)
-    print("222%s" %endTime)
-    serverConn = SmartConnectNoSSL(host=args.server,
+    logfile.write(logPrefix + "Begintime:::%s\n" %beginTime)
+    logfile.write(logPrefix + "Endtime:::%s\n" %endTime)
+
+    # Check for ssl configs and generate an appropriate ssl context object
+    ssl_verify = args.ssl_verify
+    ssl_capath = args.ssl_capath
+
+    if not ssl_verify:
+        context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+        context.verify_mode = ssl.CERT_NONE
+    elif ssl_capath:
+        context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+        context.verify_mode = ssl.CERT_REQUIRED
+        context.load_verify_locations(capath=ssl_capath)
+
+    if not ssl_verify and ssl_capath:
+        logfile.write(logPrefix + "Incorrect configuration, proceeding with "
+                       "ssl verification disabled.\n")
+    password = args.password
+    if args.pass_encrypted:
+        cipher_suite = Fernet(args.key)
+        uncipher_text = (cipher_suite.decrypt(bytes(args.password, "utf-8")))
+        password = bytes(uncipher_text).decode("utf-8") # convert to string
+
+    serverConn = SmartConnect(host=args.server,
                            user=args.user,
-                           pwd=args.password,
-                           port=args.port)
-    # serverConn = SmartConnectNoSSL(host="192.168.20.121", user=r"ESXLAB\student1", pwd="Haduc6")
+                           pwd=password,
+                           port=args.port,
+                           sslContext=context if not ssl_verify or ssl_capath else None)
     atexit.register(Disconnect, serverConn)
     separator = ",,,"
     evtCount = 0
     byTime = vim.event.EventFilterSpec.ByTime(beginTime=beginTime, endTime=endTime)
-    # eventTypeIds = [
-    #     'VirtualVmxnet2','VirtualVmxnet2Option','VirtualVmxnet3','VirtualVmxnet3Option','VirtualVmxnet3Vrdma','VirtualVmxnet3VrdmaOption','VirtualVmxnetOption','VlanProfile','PermissionAddedEvent','VmAcquiredMksTicketEvent','VmAcquiredTicketEvent','VmAutoRenameEvent','VmBeingClonedEvent','VmBeingClonedNoFolderEvent','VmBeingCreatedEvent','VmBeingDeployedEvent'
-    #     ]
-    # filterSpec = vim.event.EventFilterSpec(time=byTime, eventTypeId=eventTypeIds)
     filterSpec = vim.event.EventFilterSpec(time=byTime)
     eventManager = serverConn.content.eventManager
     eventCollector = eventManager.CreateCollectorForEvents(filterSpec)
     eventCollector.RewindCollector()
     lastReadTime = None
-    import ipdb
-    ipdb.set_trace()
     while True:
         packetContent = ""
         eventsArray = eventCollector.ReadNextEvents(maxCount=400)
         if len(eventsArray) == 0:
             break
         for event in eventsArray:
-
-            # print(event)
-            # print(json.dumps(convertToDict(event), indent=4))
-            # print(simplejson.dumps([e.__dict__ for e in event]))
-            # print(event)
-            # print(evtCount)
             evtCount += 1
-            if hasattr(event,'createdTime') and getattr(event, 'createdTime') is not None:
-                if event.createdTime.strftime('%Y-%m-%d %H:%M:%S.%f%z') == beginTime
+            if hasattr(event, 'createdTime') and getattr(event, 'createdTime') is not None:
+                if event.createdTime.strftime('%Y-%m-%d %H:%M:%S.%f%z') == beginTime:
                     continue;
                 packetContent = str(event.createdTime) + " "
                 lastReadTime = event.createdTime.strftime('%Y-%m-%d %H:%M:%S.%f%z')
-            if hasattr(event,'fullFormattedMessage') and getattr(event, 'fullFormattedMessage') is not None:
+            if hasattr(event, 'fullFormattedMessage') and getattr(event, 'fullFormattedMessage') is not None:
                 fullMsg = str(getattr(event, 'fullFormattedMessage'))
                 fullMsg = re.sub(r'[^[:ascii:]]+', r'', fullMsg)
                 fullMsg = re.sub(r'[\n]+', r'', fullMsg)
-                # print(lastReadTime)
                 packetContent = packetContent + separator + " message=" + fullMsg + separator + "user=" + event.userName
             packetContent = packetContent + separator + "eventType=" + str(type(event))
-            if hasattr(event,'vm') and getattr(event, 'vm') is not None:
+            if hasattr(event, 'vm') and getattr(event, 'vm') is not None:
                 packetContent = packetContent + separator + "vm=" + event.vm.name
-            if hasattr(event,'host') and getattr(event, 'host') is not None:
+            if hasattr(event, 'host') and getattr(event, 'host') is not None:
                 packetContent = packetContent + separator + "host=" + event.host.name
-            if hasattr(event,'datacenter') and getattr(event, 'datacenter') is not None:
+            if hasattr(event, 'datacenter') and getattr(event, 'datacenter') is not None:
                 packetContent = packetContent + separator + "datacenter=" + event.datacenter.name
-            if hasattr(event,'computeResource') and getattr(event, 'computeResource') is not None:
+            if hasattr(event, 'computeResource') and getattr(event, 'computeResource') is not None:
                 packetContent = packetContent + separator + "computeResource=" + event.computeResource.name
-            if hasattr(event,'changeTag') and getattr(event, 'changeTag') is not None:
+            if hasattr(event, 'changeTag') and getattr(event, 'changeTag') is not None:
                 packetContent = packetContent + separator + "changeTag=" + event.changeTag.name
-            """
-            if hasattr(event,'ds) and' getattr(event, 'ds') is not None:
-                packetContent = packetContent + separator + "ds=" + event.ds.name
-            if hasattr(event,'dvs') and getattr(event, 'dvs') is not None:
-                packetContent = packetContent + separator + "dvs=" + event.dvs.name
-            if hasattr(event,'net') and getattr(event, 'net') is not None:
-                packetContent = packetContent + separator + "net=" + event.net.name
-            """
-            if hasattr(event,'key') and getattr(event, 'key') is not None:
+            if hasattr(event, 'key') and getattr(event, 'key') is not None:
                 packetContent = packetContent + separator + "key=" + str(event.key)
-            if hasattr(event,'chainId') and getattr(event, 'chainId') is not None:
+            if hasattr(event, 'chainId') and getattr(event, 'chainId') is not None:
                 packetContent = packetContent + separator + "chainId=" + str(event.chainId)
 
-            if hasattr(event,'info') and getattr(event, 'info') is not None and getattr(event.info, 'error') is not None:
+            if hasattr(event, 'info') and getattr(event, 'info') is not None and getattr(event.info, 'error') is not None:
                 packetContent = packetContent + separator + "error=" + str(event.info.error)
 
             packetContent = packetContent + "\n"
 
             if outputFile is not None:
-                # print(packetContent)
                 outputFile.write(packetContent)
-                # outputFile.write(str(evtCount))
-                #  outputFile.write(str(event))
-                # outputFile.write(json.dumps(convertToDict(event, outputFile), indent=4))
-                # print(event.__dict__.keys())
-                # json.dump(event.__class__.__name__,outputFile
-                # obj.__dict__.dump(fullMsg, outputFile)
             if targetSocket is not None:
                 targetSocket.send(packetContent)
 
-    print(logPrefix + "%s events collected." % (evtCount))
+    logfile.write(logPrefix + "%s events collected.\n" % (evtCount))
 
     if lastReadTime is not None:
-        print("%sUpdating timelog with %s ..." % (logPrefix, lastReadTime))
-        updateLastReadTime(lastReadTime, tsFileName)
-        print(" Done, exiting.")
+        logfile.write(logPrefix + " Updating timelog with %s ...\n" % (lastReadTime))
+        updateLastReadTime(lastReadTime, args.timestampFile)
+        logfile.write(logPrefix + " Done, exiting.\n")
     elif evtCount != 0:
-        print("Error: no last read time")
+        logfile.write(logPrefix + "Error: no last read time\n")
+
+    if logfile is not None:
+        logfile.close()
 
     if outputFile is not None:
         outputFile.close()
@@ -297,7 +271,6 @@ def main():
     if targetSocket is not None:
         targetSocket.close()
 
-    # serverConn.disconnect()
 
 if __name__ == '__main__':
     main()
